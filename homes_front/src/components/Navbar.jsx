bfa6744cfcadda8546398d07d3223c61104aa1ed
ievas <ieva.ritina@gmail.com>
2024-04-21 01:04:36 -0600

    header and search bar improved

--- homes_front/src/components/Navbar.jsx
+++ homes_front/src/components/Navbar.jsx
@@ -7,22 +7,24 @@ function Navbar({ isLoggedIn, setIsLoggedIn }) {
 
   return (
     <>
-      {isLoggedIn ? (
-        <>
-          <Link to="/">Homes</Link>
-          <Link to="/create">Create</Link>
-          <Link to="/account">Inventory</Link>
-          <button id="logout" onClick={handleLogout}>
-            Logout
-          </button>
-        </>
-      ) : (
-        <>
-          <Link to="/">Homes For Sale</Link>
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/signin">Sign In</Link>
-        </>
-      )}
+      <div className="navbar">
+        {isLoggedIn ? (
+          <>
+            <Link to="/">Homes</Link>
+            <Link to="/create">Create</Link>
+            <Link to="/account">Inventory</Link>
+            <button id="logout" onClick={handleLogout}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <Link to="/">Homes For Sale</Link>
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/signin">Sign In</Link>
+          </>
+        )}
+      </div>
     </>
   );
 }
